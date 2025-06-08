162eab03076ee2e4d26384e828f23a4acb8f4e6a
ltoledo-dev <luisfertoledo74@gmail.com>
2025-06-08 13:29:34 -0600

    fix(user): DummyUser for login

--- src/CBM_SOLICITUDES/Presentation/WebApi/Functionalities/Authentication/Controllers/AuthenticationController.ts
+++ src/CBM_SOLICITUDES/Presentation/WebApi/Functionalities/Authentication/Controllers/AuthenticationController.ts
@@ -4,16 +4,13 @@ import { generateToken } from '../../../Utils/JWT';
 export const login = async (req: Request, res: Response): Promise<void> => {
     const { usuario, clave } = req.body;
 
-    // Aquí haces la validación contra una "base de datos" o valores simulados
     if (usuario === 'admin' && clave === 'admin') {
         const user = { id: 1, username: usuario }; // Usuario simulado
         const token: string = generateToken({ userId: user.id, username: user.username });
 
-        // Enviar token por header
         res.setHeader('Authorization', `Bearer ${token}`);
         res.setHeader('Access-Control-Expose-Headers', 'Authorization');
 
-        // También puedes enviar el token en el cuerpo si quieres
         res.status(200).json();
     } else {
         res.status(401).json({ message: 'Credenciales inválidas' });
