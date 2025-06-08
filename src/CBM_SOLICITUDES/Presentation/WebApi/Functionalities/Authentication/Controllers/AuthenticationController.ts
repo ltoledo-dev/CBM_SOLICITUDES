54da3f0283fd1a35c105ef3dbc4a8badc50295bd
ltoledo-dev <luisfertoledo74@gmail.com>
2025-06-08 13:29:13 -0600

    fix(user): DummyUser for login

--- src/CBM_SOLICITUDES/Presentation/WebApi/Functionalities/Authentication/Controllers/AuthenticationController.ts
+++ src/CBM_SOLICITUDES/Presentation/WebApi/Functionalities/Authentication/Controllers/AuthenticationController.ts
@@ -5,7 +5,7 @@ export const login = async (req: Request, res: Response): Promise<void> => {
     const { usuario, clave } = req.body;
 
     // Aquí haces la validación contra una "base de datos" o valores simulados
-    if (usuario === 'ltoledo' && clave === '123') {
+    if (usuario === 'admin' && clave === 'admin') {
         const user = { id: 1, username: usuario }; // Usuario simulado
         const token: string = generateToken({ userId: user.id, username: user.username });
 
